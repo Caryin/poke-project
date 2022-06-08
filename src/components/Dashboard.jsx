f72bc18c50c81c3d57b5b571a410e4c5ade9d26b
Caryin <yeecaryin@gmail.com>
2022-06-08 15:09:11 +0800

    import converted csv data to json

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import pokemonData from '../store/pokemon.json';
+
+//chakraUI
+import { Text } from '@chakra-ui/react';
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+  console.log(pokemonData);
+
+  return (
+    <div>
+      <Text fontSize='2xl'>Dashboard</Text>
+      <div>{JSON.stringify(pokemonData)}</div>
+    </div>
+  );
 }
 
 export default Dashboard;
