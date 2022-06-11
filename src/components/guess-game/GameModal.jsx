3297cfe727fe14d5b6bc182e4ee33e3def42f8b5
Caryin <yeecaryin@gmail.com>
2022-06-11 22:43:26 +0800

    refactor

--- src/components/guess-game/GameModal.jsx
+++ src/components/guess-game/GameModal.jsx
@@ -1,4 +1,11 @@
-import { Button, Heading, Input, Text, VStack } from '@chakra-ui/react';
+import {
+  Button,
+  Heading,
+  Input,
+  Text,
+  useToast,
+  VStack,
+} from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import { GAMESETTING } from './gameSetting';
 
@@ -7,6 +14,7 @@ const GameModal = ({ guessGameSuccessful, onClose, gameLevel }) => {
   const [numOfAttempts, setNumOfAttempts] = useState(3);
   const [secretNumber, setSecretNumber] = useState(1);
   const [userInput, setUserInput] = useState('');
+  const toast = useToast();
 
   useEffect(() => {
     getSecretNumber();
@@ -28,6 +36,18 @@ const GameModal = ({ guessGameSuccessful, onClose, gameLevel }) => {
     }
 
     let userGuess = parseInt(userInput);
+
+    if (userGuess < range.min || userGuess > range.max) {
+      toast({
+        title: 'Out of range!',
+        description: ` Make a guess between ${range.min} and ${range.max}`,
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      });
+      return;
+    }
+
     if (userGuess === secretNumber) {
       guessGameSuccessful();
       onClose();
@@ -55,6 +75,7 @@ const GameModal = ({ guessGameSuccessful, onClose, gameLevel }) => {
             w='50%'
             onChange={userInputHandler}
             value={userInput}
+            textAlign='center'
           />
           <Button w='20%' colorScheme='red' onClick={attemptHandler}>
             Try
